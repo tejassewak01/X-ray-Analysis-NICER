695eeae5cfb693aaf253fed34a67e7e30344b815
tejassewak01 <153519299+tejassewak01@users.noreply.github.com>
2025-02-19 22:01:00 -0600

    Script - To create individual GTI files for NICER data

--- scripts/sc_gti_creator.py
+++ scripts/sc_gti_creator.py
@@ -3,13 +3,20 @@ import numpy as np
 
 # A obsid directory name can be decomposed in two parts: obsid_constant and obsid_variable./
 
-obsid_constant =     # For example 57060101 is the obsid_constant for a directory 5706010101 and 01 wil be obsid_variable
+obsid_constant = 566001  # For example 57060101 is the obsid_constant for a directory 5706010101 and 01 wil be obsid_variable
 
-range1, range2 =    # Range for obsid_variable - Example: 1,30
+
+# Now, there are two options, either give range or directly list out all the obsid_variables
+# Here I have listed out all the obsid_variables and commented out the range method. 
+
+#range1, range2 =    # Range for obsid_variable - Example: 1,30
+
+obsid_variables = [] # Note if your variable is 01, just write 1 as padding (01) will be done later. 
+#Example list [1,2,3,4,5,6,7,8,9,10,11,...30]. Helpful if example list is like [101,201,301,401,501,601,701,801,901,1001,1101,1201,...3001]
 
 length_of_obsid_variable =     # For example if variable in obsid file is 01,02,03,...30 - then keep length_of_obsid_variable = 2
 
-for obsid_variable in range(range1, range2):
+for obsid_variable in obsid_variables: # CHANGE if using the range method
 
     obsid_variable = f'{obsid_variable:0{length_of_obsid_variable}}'  # Format obsid_variable as two digits so that 1 is read as 01.
 
@@ -38,5 +45,4 @@ for obsid_variable in range(range1, range2):
         print(f"GTI file created: {gti_filename}")
 
     # Close the FITS files
-    hdul_mkf.close()
     hdul_lc.close()
